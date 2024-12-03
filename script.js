20c6dd8760b248b97039ecb5895c5fdbd7588992
jehadjoy55 <joyjehad28@gmail.com>
2024-12-03 22:15:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const displayedImage = document.getElementById('displayedImage');
 const nextButton = document.getElementById('nextButton');
 
-// Tracks the current image index
 let currentImageIndex = 1;
 
 nextButton.addEventListener('click', () => {
+    // Toggle between image 1 and image 2
     currentImageIndex = currentImageIndex === 1 ? 2 : 1;
     displayedImage.src = `${currentImageIndex}.jpg`;
 });
